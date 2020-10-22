db0ff51fb57e732694cb34a943cc8701b30bc898
Lycoris <podsipanina@gmail.com>
2020-10-22 18:47:28 +0300

    Updated .json format

--- test_project/korablik_test.py
+++ test_project/korablik_test.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
-import unicodecsv as csv
+import json
 
-#   CSV = 'Childrens.csv'
-HOST = 'https://korablik-fond.ru/'
-URL_O = 'https://korablik-fond.ru/our-children/'
-URL_C = 'https://korablik-fond.ru/contacts/'
-URL_R = 'https://korablik-fond.ru/reporting/'
-URL_A = 'https://korablik-fond.ru/about/S'
+
+BASE_HOST = 'https://korablik-fond.ru/'
+CHILD_HOST = 'our-children/'
+CONT_HOST = 'contacts/'
+REP_HOST = 'reporting/'
+ABOUT_HOST = 'about/'
 HEADERS = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'
@@ -16,7 +16,7 @@ HEADERS = {
 
 def get_html(url, params=''):
     r = requests.get(url, headers=HEADERS, params=params)
-    #   print(type(r))
+    #   print(dir(r))
     return r
 
 
@@ -27,35 +27,20 @@ def get_all_content(html):
     child = []
     #   print(pg.prettify())
     for it in items:
-        child.append(
+        child.append(json.dumps(
             {
                 'name': it.find('h4').get_text(strip=True),
                 'info': [p.get_text(strip=True) for p in it.findAll('p')],  # it.find('p').get_text(),
-                'link': HOST + it.find('div', class_='leyka-scale-button').find('a').get('href'),
+                'link': BASE_HOST + it.find('div', class_='leyka-scale-button').find('a').get('href'),
                 'money': [div.get_text() for div in it.findAll('div', class_='leyka-scale-label')]
-                # it.find('div', class_='leyka-scale-label'),
                 # 'img': HOST + it.find('div', class_='tpl-pictured-bg').get('style')
-            }
+            }, sort_keys=False, indent=4, ensure_ascii=False, separators=(',', ': '))
         )
-    #   print('Type of "name","info" etc: ', type('link'))
     return child
-
-
-"""def save_data(items, path):   # Запись данных в csv файл, пока не работает :/
-    with open(path, mode='w+', newline='', encoding='utf-8') as file:
-        #   colum = ['Имя ребёнка', 'Информация', 'Ссылка', 'Собрано средства']
-        writer = csv.writer(file, delimiter=';')     #  , lineterminator='\r', fieldnames=colum)
-        #   writer.writeheader()
-        #   writer.writerow(['Имя ребёнка', 'Информация', 'Ссылка', 'Собрано средства'])
-        for item in items:
-            writer.writerow([item.__str__('name'), item.__str__('info'), item.__str__('link'), item.__str__('money')])
-"""
 
 
 def get_child(url, num):
     n = parser(url)
-    #   count = len(n)
-    #   print(type(n))
     print(n[num-1])
 
 
@@ -64,11 +49,13 @@ def get_contacts(url):
     cont = sp.find('div', class_='col-md-7').find('ul')
     # print(cont)
     tmp = cont.findAll('li')
-    #   print(len(tmp))
-    phone = tmp[0].find('span').get_text() + tmp[0].find('a').get('href'),
-    mail = tmp[1].find('span').get_text() + tmp[1].find('a').get('href'),
-    adress = tmp[2].find('span').get_text() + tmp[2].find('a').get('href')
-    print('\t', phone, '\n\t', mail, '\n\t', adress)
+    print(type(tmp))
+    contact = json.dumps({
+        'phone': tmp[0].find('span').get_text() + tmp[0].find('a').get('href'),
+        'mail': tmp[1].find('span').get_text() + tmp[1].find('a').get('href'),
+        'adres': tmp[2].find('span').get_text() + tmp[2].find('a').get('href')
+    }, sort_keys=False, indent=4, ensure_ascii=False, separators=(',', ': '))
+    print(contact)
 
 
 def get_reports(url):
@@ -80,14 +67,8 @@ def get_about(url):
 
 
 def parser(url, params=''):
-    #html = get_html(URL_O)
-    #if html.status_code == 200:
     child = []
     child.extend(get_all_content(url.text))
-        #   save_data(child, CSV)
-        #   print(child)
-    #else:
-    #    print('Error')
     return child
 
 
@@ -106,13 +87,13 @@ def menu():
     case = input('\tВыберете пункт меню: ')
     # case = int(case.strip())
     if case == '1':
-        html = get_html(URL_O)
+        html = get_html(BASE_HOST+ CHILD_HOST)
         if html.status_code == 200:
             print('\n'.join(map(str, parser(html))))
         else:
             print('Error status code')
     elif case == '2':
-        html = get_html(URL_O)
+        html = get_html(BASE_HOST + CHILD_HOST)
         if html.status_code == 200:
             patc = parser(html)
             length = len(patc)
@@ -123,7 +104,7 @@ def menu():
         else:
             print('Error status code')
     elif case == '3':
-        r = get_html(URL_C)
+        r = get_html(BASE_HOST + CONT_HOST)
         if r.status_code == 200:
             html = r.content
             get_contacts(html)
@@ -131,13 +112,13 @@ def menu():
             print('Error status code')
         #   print(parser())
     elif case == '4':
-        html = get_html(URL_R)
+        html = get_html(BASE_HOST + REP_HOST)
         if html.status_code == 200:
             get_reports(html)
         else:
             print('Error status code')
     elif case == '5':
-        html = get_html(URL_A)
+        html = get_html(BASE_HOST + ABOUT_HOST)
         if html.status_code == 200:
             get_about(html)
         else:
